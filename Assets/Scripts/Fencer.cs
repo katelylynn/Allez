f79d62c63e31d93609413864cb9b2b3b1d28bd5d
kate <katezraly@gmail.com>
2025-10-01 15:04:49 -0700

    placed fencers in starting pos on piste

--- Assets/Scripts/Fencer.cs
+++ Assets/Scripts/Fencer.cs
@@ -2,9 +2,25 @@ using UnityEngine;
 
 public class Fencer : MonoBehaviour
 {
+    // static fields
+    private const int F0 = 0;
+    private const int F1 = 1;
+
+    // instance fields
     private int fencerId;
     private Camera cam;
 
+    // starting position
+    private Vector3[] startingPos = {
+        new Vector3(0, 0, -5),
+        new Vector3(0, 0, 5)
+    };
+    private Quaternion[] startingRot = {
+        Quaternion.Euler(0f, 0f, 0f),
+        Quaternion.Euler(0f, 180f, 0f)
+    };
+
+    // called by scene initializer
     public void Initialize(int fn)
     {
         // set id
@@ -13,7 +29,13 @@ public class Fencer : MonoBehaviour
         // set camera position
         cam = GetComponentInChildren<Camera>(); 
         Rect r = cam.rect;
-        r.x = (fencerId == 1 ? 0f : 0.5f);
+        r.x = (fencerId == F0 ? 0f : 0.5f);
         cam.rect = r;
+
+        // set fencer position, deactivating to overcome rigidbody
+        gameObject.SetActive(false);
+        gameObject.transform.position = startingPos[fencerId];
+        gameObject.transform.rotation = startingRot[fencerId];
+        gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/SceneInitializer.cs
+++ Assets/Scripts/SceneInitializer.cs
@@ -5,18 +5,22 @@ public class SceneInitializer : MonoBehaviour
 {
     public GameObject gameManagerPrefab;
     public GameObject fencerPrefab;
+    public GameObject environmentPrefab;
 
     void Awake()
     {
         GameObject g = Instantiate(gameManagerPrefab);
         g.name = "GameManager";
 
+        GameObject f0 = Instantiate(fencerPrefab);
+        f0.name = "Fencer0";
+        f0.GetComponent<Fencer>().Initialize(0);
+
         GameObject f1 = Instantiate(fencerPrefab);
         f1.name = "Fencer1";
         f1.GetComponent<Fencer>().Initialize(1);
 
-        GameObject f2 = Instantiate(fencerPrefab);
-        f2.name = "Fencer2";
-        f2.GetComponent<Fencer>().Initialize(2);
+        GameObject env = Instantiate(environmentPrefab);
+        env.name = "Environment";
     }
 }

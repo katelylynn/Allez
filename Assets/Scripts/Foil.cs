8f881bf9a671eda9ec761d8fc957bed3f87907ff
kate <katezraly@gmail.com>
2025-10-11 16:25:45 -0700

    added torso hit

--- Assets/Scripts/Foil.cs
+++ Assets/Scripts/Foil.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class Foil : MonoBehaviour
 {
+    public Fencer fencer;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Foil"))
         {
             EventManager.TriggerParrySuccess();
+        }
+        else if (other.CompareTag("Torso"))
+        {
+            // successfully hitting your opponent's torso ends the round
+            EventManager.TriggerRoundEnd(fencer.fencerId);
         }
     }
 }

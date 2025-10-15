6644668c5e2a2a1185bceff44cf8e27118e5b7f6
kapybara <97310663+Kimmm-c@users.noreply.github.com>
2025-10-14 23:03:49 -0700

    hooked the parried animation to the CombatManager script

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -5,12 +5,15 @@ public class CombatManager : MonoBehaviour
     private Fencer fencer0;
     private Fencer fencer1;
 
+    // I'm considering a better way to organize all the animation layers and their indices
+    private int foilLayerIndex = 1;
+
     public void Start()
     {
         EventManager.ParrySuccess += HandleParrySuccess;
     }
 
-    public void Initialize(Fencer f0, Fencer f1)
+    public void Initialize( Fencer f0, Fencer f1 )
     {
         fencer0 = f0;
         fencer1 = f1;
@@ -18,9 +21,17 @@ public class CombatManager : MonoBehaviour
 
     private void HandleParrySuccess()
     {
-        if (fencer0.GetStateSnapshot(1).IsName("ParryLeft"))
-            Debug.Log("Fencer 0 parries Fencer 1!");
-        else if (fencer1.GetStateSnapshot(1).IsName("ParryLeft"))
-            Debug.Log("Fencer 1 parries Fencer 0!");
+        if ( fencer0.GetStateSnapshot( 1 ).IsName( "ParryLeft" ) )
+        {
+            Debug.Log( "fencer 0 parries Fencer 1!" );
+            Animator f1Animator = fencer1.GetComponent<Animator>();
+            f1Animator.Play( "Parried", foilLayerIndex, 0f );
+        }
+        else if ( fencer1.GetStateSnapshot( 1 ).IsName( "ParryLeft" ) )
+        {
+            Debug.Log( "Fencer 1 parries Fencer 0!" );
+            Animator f0Animator = fencer0.GetComponent<Animator>();
+            f0Animator.Play( "Parried", foilLayerIndex, 0f );
+        }
     }
 }

55be23b1b8a717a36e55257b793ae9d95506a39a
kate <katezraly@gmail.com>
2025-11-04 14:53:07 -0800

    added sword clash

--- Assets/Scripts/Foil.cs
+++ Assets/Scripts/Foil.cs
@@ -1,14 +1,20 @@
-using System.Collections;
 using UnityEngine;
 
 public class Foil : MonoBehaviour
 {
-    public Fencer fencer;
+    public ParticleSystem clashEffect;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Foil"))
         {
+            // Play clash particle
+            Vector3 contact = other.ClosestPoint(transform.position);
+            ParticleSystem effect = Instantiate(clashEffect, contact, Quaternion.identity);
+            Debug.Log($"⚔️ Clash at position: {contact}");
+            Destroy(effect.gameObject, effect.main.duration);
+
+            // Trigger parry success event
             EventManager.TriggerParrySuccess();
         }
     }

f4409db52e904396eb743ddbe8041a52dd17341f
Inez Yoon <inez3cs@gmail.com>
2025-11-13 11:34:20 -0800

    Finalize merge

--- Assets/Scripts/UIScripts/RoundStartCountdown.cs
+++ Assets/Scripts/UIScripts/RoundStartCountdown.cs
@@ -13,9 +13,6 @@ public class RoundStartCountDown : MonoBehaviour
     [Header("UI")]
     public TMP_Text roundWinner;
 
-    [Header("SFX")]
-    [SerializeField] private AudioClip ouch1Clip; // P1
-    [SerializeField] private AudioClip ouch2Clip; // P2
 
     private TMP_Text[] countdownTexts;
     private float countdownTime = 3f;
@@ -65,17 +62,8 @@ public class RoundStartCountDown : MonoBehaviour
 
     public void DisplayWinner(int winner)
     {
-        // pick clip based on winner
-        AudioClip clip = (winner == 0) ? ouch1Clip : ouch2Clip;
 
-        if (clip == null)
-        {
-            Debug.LogWarning($"[RoundStartCountDown] Winner clip is NULL (winner={winner}). Assign ouch1Clip/ouch2Clip in Inspector.");
-        }
-        else if (audioSource != null)
-        {
-            audioSource.PlayOneShot(clip);
-        }
+
 
         if (roundWinner != null)
         {
@@ -87,90 +75,3 @@ public class RoundStartCountDown : MonoBehaviour
 
     public void HideWinner() => roundWinner?.gameObject.SetActive(false);
 }
-
-// using System.Collections;
-// using TMPro;
-// using Unity.VisualScripting;
-// using UnityEngine;
-
-// public class RoundStartCountDown : MonoBehaviour
-// {
-//     public float countdownTickDuration = 1.5f;
-//     public Transform textParent;
-//     private TMP_Text[] countdownTexts;
-//     private float countdownTime = 3f;
-//     public TMP_Text roundWinner;
-    
-//     // Audio sources : don't need audio source with extra object with AudioClip : needs to be cleaend up later
-//     public AudioSource countdownAudio;
-//     public AudioClip ouch1Clip;
-//     public AudioClip ouch2Clip;
-//     private AudioSource audioSource;
-
-//     void Start() {
-//         audioSource = GetComponent<AudioSource>();
-//     }
-
-//     public IEnumerator Run()
-//     {
-//         countdownTexts = textParent.GetComponentsInChildren<TMP_Text>(true);
-
-//         //Wait a short time for loading to sync up audio
-//         yield return null;
-//         yield return new WaitForEndOfFrame();
-//         yield return StartCoroutine(Countdown());
-//     }
-
-//     public IEnumerator Countdown()
-//     {
-        
-//         float count = countdownTime;
-//         while (count > 0)
-//         {
-            
-//             int i = Mathf.RoundToInt(countdownTime - count);
-
-//             foreach (var t in countdownTexts)
-//                 t.gameObject.SetActive(false);
-
-//             if (i >= 0 && i < countdownTexts.Length)
-//                 countdownTexts[i].gameObject.SetActive(true);
-//             if (!countdownAudio.isPlaying)
-//             {
-//                 countdownAudio.Play();
-//             }
-//             yield return new WaitForSeconds(countdownTickDuration);
-//             count--;
-//         }
-
-//         foreach (var t in countdownTexts)
-//             t.gameObject.SetActive(false);
-//     }
-
-//     public void DisplayWinner(int winner)
-//     {
-//         string winnerText;
-
-//         if (winner == 0)
-//         {
-//             audioSource.PlayOneShot(ouch1Clip);
-//             winnerText = "Player one";
-//             roundWinner.color = Color.blue;
-//         }
-//         else
-//         {
-//             audioSource.PlayOneShot(ouch2Clip);
-//             winnerText = "Player two";
-//             roundWinner.color = Color.red;
-//         }
-//         winnerText += " scores a touch!";
-
-//         roundWinner.text = winnerText;  
-//         roundWinner.gameObject.SetActive(true);
-//     }
-
-//     public void HideWinner()
-//     {
-//         roundWinner.gameObject.SetActive(false);
-//     }
-// }

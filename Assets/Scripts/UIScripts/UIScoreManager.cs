a247eebc8e02ba025d436f525ae68a4fe793266d
taylor.ockerman <taylor.ockerman@gmail.com>
2025-11-12 16:36:38 -0800

    added players names to main scene ui

--- Assets/Scripts/UIScripts/UIScoreManager.cs
+++ Assets/Scripts/UIScripts/UIScoreManager.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIScoreManager : MonoBehaviour
 {
     public GameObject winPip;
     public GameObject pip;
     public GameManager gm;
+    public TMP_Text playerName;
     private Transform scoreUI;
     public bool isP1 = true;
     private bool p1Scored;
     private bool p2Scored;
     private int[] oldScore = {0,0};
+    PlayerDataManager dm;
 
     public void Initialize(GameObject gameManager)
     {
         gm = gameManager.GetComponent<GameManager>();
+        dm = PlayerDataManager.GetInstance();
         scoreUI = transform.Find("PipContainer");
     }
 
@@ -29,7 +33,7 @@ public class UIScoreManager : MonoBehaviour
         p2Scored = oldScore[1] != PlayerPrefs.GetInt("P2Score");
         oldScore[0] = PlayerPrefs.GetInt("P1Score");
         oldScore[1] = PlayerPrefs.GetInt("P2Score");
-
+        playerName.text = isP1 ? dm.p1 : dm.p2;
         int playerScore = isP1 ? PlayerPrefs.GetInt("P1Score") : PlayerPrefs.GetInt("P2Score");
         for (int i = 0; i < gm.pointsToWin; i++)
         {

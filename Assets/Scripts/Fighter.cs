704028c9f3bd18a69c337df83c59c5db59586f70
Navjot Kehal <navjotkehal5@gmail.com>
2025-11-19 11:45:44 -0800

    Implemented parrying action using multi aim constraint

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 using UnityEngine.InputSystem;
 
 public class Fighter : MonoBehaviour
@@ -19,8 +20,6 @@ public class Fighter : MonoBehaviour
     private Coroutine currentTiltCoroutine;
     private Coroutine currentParryCoroutine;
 
-    private bool isTilting = false;
-
     public void Start()
     {
         anim = GetComponent<Animator>();
@@ -29,11 +28,13 @@ public class Fighter : MonoBehaviour
     public void OnAttack(InputValue value)
     {
         if (!anim.GetBool("Parry"))
-        anim.SetTrigger("Attack");
+            anim.SetTrigger("Attack");
     }
 
     public void OnTilt(InputValue tiltDirection)
     {
+        // need to check if any triggers are being set before doing this
+        // can only not do this if player is lunging
         float tilt = tiltDirection.Get<float>();
 
         if (currentTiltCoroutine != null)
@@ -58,7 +59,6 @@ public class Fighter : MonoBehaviour
         float time = 0;
         Vector3 startPos = ParryTracker.localPosition;
         Vector3 targetPos = new Vector3(targetLocalX, startPos.y, startPos.z);
-        isTilting = true;
 
         while (time < 1)
         {
@@ -67,19 +67,21 @@ public class Fighter : MonoBehaviour
             yield return null;
         }
 
-        isTilting = false;
         ParryTracker.localPosition = targetPos; // Snap to final position
     }
 
     public void OnParry(InputValue parryDirection)
     {
-        // do the parry action
-        if (currentParryCoroutine == null && !isTilting)
+        // need to check if other triggers are being set
+        // can only do this if player is not attacking, lunging, or backdashing
+        if (currentParryCoroutine == null)
         {
             float parryDir = parryDirection.Get<float>();
+
             GameObject Rig1 = ParryTracker.parent.gameObject;
-            //Rig1.transform.GetChild(0).gameObject.SetActive(false);
-            //Rig1.transform.GetChild(3).gameObject.SetActive(true);
+            Transform child = Rig1.transform.GetChild(0);
+            MultiAimConstraint aimConstraint = child.GetComponent<MultiAimConstraint>();
+            aimConstraint.weight = 0f;
 
             if (parryDir == -1)
             {
@@ -111,14 +113,22 @@ public class Fighter : MonoBehaviour
         }
 
         ParryTracker.localPosition = targetPos;
-        GameObject Rig1 = ParryTracker.parent.gameObject;
-        //Rig1.transform.GetChild(0).gameObject.SetActive(true);
-        //Rig1.transform.GetChild(3).gameObject.SetActive(false);
         currentParryCoroutine = null;
 
+        // Ensures that the sword goes back to its original position
         if (isReversing)
+        {
             currentParryCoroutine = StartCoroutine(DoParry(-direction));
-        
-        anim.SetBool("Parry", false);
+        }
+
+        if (currentParryCoroutine == null)
+        {
+            GameObject Rig1 = ParryTracker.parent.gameObject;
+            Transform child = Rig1.transform.GetChild(0);
+            MultiAimConstraint aimConstraint = child.GetComponent<MultiAimConstraint>();
+            aimConstraint.weight = 1f;
+
+            anim.SetBool("Parry", false);
+        }
     }
 }

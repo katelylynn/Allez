55da4de9b49850e2eabdbec476c39d8a44bc621f
taylor.ockerman <taylor.ockerman@gmail.com>
2025-12-06 00:10:41 -0800

    Scoring particle effect will now only play for the player who touched first

--- Assets/Scripts/FoilAttack.cs
+++ Assets/Scripts/FoilAttack.cs
@@ -3,20 +3,36 @@ using UnityEngine;
 public class FoilAttack : MonoBehaviour
 {
     public Fencer fencer;
+    private static bool hitRegistered = false;
+    private ParticleSystem ps;
+    private void Awake()
+    {
+        ps = GetComponentInChildren<ParticleSystem>();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Torso"))
-        {
-            EventManager.TriggerRoundEnd(fencer.fencerId);
-            //gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
-            gameObject.GetComponentInChildren<ParticleSystem>().Play();
-            gameObject.GetComponentInChildren<ParticleSystem>().Simulate(Time.unscaledDeltaTime, true, false);
-        }
+        if (!other.CompareTag("Torso")) return;
+
+        // if someone already scored, ignore further hits
+        if (hitRegistered) return;
+
+        hitRegistered = true;
+
+        EventManager.TriggerRoundEnd(fencer.fencerId);
+        //Debug.Log("should play");
+        ps.Play();
+        ps.Simulate(Time.unscaledDeltaTime, true, false);
+
     }
 
     private void OnTriggerExit(Collider other)
     {
         //Debug.Log("trigger exit");
         //gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+    }
+
+    public static void ResetHit()
+    {
+        hitRegistered = false;
     }
 }
